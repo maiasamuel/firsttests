8df86ebeca9c17da2e670c6cf421e8de5657bfba
Maia Samuel <maiasamuel@Maias-MacBook-Air.local>
2016-09-06 13:27:08 -0600

    initial

---
+++ test/canary-spec.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+
+const should = chai.should();
+
+describe('Canary test', ()=> {
+  it('the string hello should be hello', ()=> {
+    const hello = 'hello';
+
+    hello.should.be.equal('hello');
+  });
+
+  it('typeof string should be a string', ()=> {
+    const str = 'maia';
+
+    str.should.be.a('string');
+  });
+});
