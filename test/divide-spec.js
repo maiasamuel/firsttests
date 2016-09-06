e981dded49a2d345eb708d00be78a43e5a9eeb9e
Maia Samuel <maiasamuel@Maias-MacBook-Air.local>
2016-09-06 15:23:25 -0600

    divide

---
+++ test/divide-spec.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const supertest = require('supertest');
+
+const app = require('../app.js');
+
+const should = chai.should();
+const api = supertest(app);
+
+describe('Sending a POST to /api/divide', ()=> {
+  describe('should succeed', ()=> {
+    it('in dividing two numbers together', (done)=> {
+      api.post('/api/divide')
+        .send({
+          num1:10,
+          num2:2
+        })
+        .expect(200)
+        .end((err, res)=> {
+          if(err) return done(err);
+
+          res.body.result.should.be.equal(5);
+
+          done();
+        });
+    });
+  });
+
+  describe('should fail', ()=> {
+    it('when the nothing is sent in', (done)=> {
+      api.post('/api/divide')
+        .expect(432)
+        .end((err, res)=> {
+          if(err) return done(err);
+
+          res.body.message.should.be.equal('No data');
+
+          done();
+    });
+  });
+});
+});
