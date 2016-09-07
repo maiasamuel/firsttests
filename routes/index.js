ca72497aaf23ac6dcad22352aaa72788d51846f0
Maia Samuel <maiasamuel@Maias-MacBook-Air.local>
2016-09-07 10:39:11 -0600

    small change

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var app = express();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+app.listen(4500, function() {
+  console.log('Listening on port 4500');
+});
